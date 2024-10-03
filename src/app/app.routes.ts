be987a349744f236a8f88fe756615e52d94db819
Christian Frei <christian.frei.01@googlemail.com>
2024-10-03 17:29:20 +0200

    add Material Symbols font and create main component, chat, sidenav and main header, change background color of body

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LogInComponent } from './log-in/log-in.component';
+import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
     { path: '', component: LogInComponent },
+    { path: 'main', component: MainComponent },
 ];
