a080467413eea85319cdb04ecb306f34817a0a86
Christian Frei <christian.frei.01@googlemail.com>
2024-10-03 23:57:46 +0200

    work on structure of main app section

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { ChatComponent } from './chat/chat.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { ThreadComponent } from './thread/thread.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent, ChatComponent],
+  imports: [HeaderComponent, SideNavComponent, ChatComponent, ThreadComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
