5444281efda9651daf24e3bcfa2e937d306c95cd
Christian Frei <christian.frei.01@googlemail.com>
2024-10-04 21:31:45 +0200

    start implementing thread feature and fix icon button

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { ChatComponent } from './chat/chat.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { ThreadComponent } from './thread/thread.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent, SideNavComponent, ChatComponent, ThreadComponent],
+  imports: [HeaderComponent, SideNavComponent, ChatComponent, ThreadComponent, CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+  @ViewChild(ThreadComponent) thread!: ThreadComponent;
+  hidden: boolean = false;
 
+  changeHide(bool: boolean) {
+    this.hidden = bool;
+  }
 }
--- src/app/main/thread/thread.component.ts
+++ src/app/main/thread/thread.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Message } from '../../models/message.class';
+import { MessageComponent } from '../message/message.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-thread',
   standalone: true,
-  imports: [],
+  imports: [MessageComponent, CommonModule],
   templateUrl: './thread.component.html',
   styleUrl: './thread.component.scss'
 })
 export class ThreadComponent {
-
+  public hidden: boolean = false;
+  @Output() hide = new EventEmitter<boolean>();
+  emitClose() {
+    this.hide.emit(true);
+  }
+  channelName: string = 'Entwicklerteam';
+  messages: Message[] = [
+    new Message('avatar4.svg', 'Maria Musterfrau', 1, '00:00', '00:00', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi odio quia distinctio, a rem tenetur nihil iste saepe voluptates.'),
+    new Message(),
+    new Message(),
+    new Message()
+  ];
 }
