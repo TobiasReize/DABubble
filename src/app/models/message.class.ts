c2d699f7e7414d461ba85b47a1e0273923f47bd3
Christian Frei <christian.frei.01@googlemail.com>
2024-10-05 19:26:06 +0200

    start adding reactions

--- src/app/models/message.class.ts
+++ src/app/models/message.class.ts
@@ -1,3 +1,5 @@
+import { Reaction } from "./reaction.class";
+
 export class Message {
     constructor(
         public imageName: string = 'avatar1.svg',
@@ -5,6 +7,7 @@ export class Message {
         public numberOfReplies: number = 0,
         public postedAt: string = '00:00',
         public lastReplyAt: string = '00:00',
-        public content: string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi odio quia distinctio, a rem tenetur nihil iste saepe voluptates.'
+        public content: string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi odio quia distinctio, a rem tenetur nihil iste saepe voluptates.',
+        public reactions: Reaction[] = [new Reaction('avatar2.svg', ['Maria Mustermann']), new Reaction]
     ) {}
 }
---
+++ src/app/models/reaction.class.ts
@@ -0,0 +1,6 @@
+export class Reaction {
+    constructor(
+        public emoji: string = 'avatar1.svg',
+        public userNames: string[] = ['Max Mustermann', 'Maria Musterfrau']
+    ) {}
+}
