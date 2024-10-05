e5a39cc55c9259ec85dc2053a638a3768810896a
KristianCebic <kristian.cebic13@gmail.com>
2024-10-05 19:07:56 +0200

    side-nav update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LogInComponent } from './log-in/log-in.component';
 import { MainComponent } from './main/main.component';
+import { SideNavComponent } from './main/side-nav/side-nav.component';
 
 export const routes: Routes = [
     { path: '', component: LogInComponent },
     { path: 'main', component: MainComponent },
+    { path: 'side-nav', component: SideNavComponent },
 ];
