99771cc57576c1abb1bc48484239f338c90598ac
Christian Frei <christian.frei.01@googlemail.com>
2024-10-06 17:09:43 +0200

    Add horizontal rule to thread and change Message class

--- src/app/models/message.class.ts
+++ src/app/models/message.class.ts
@@ -8,10 +8,18 @@ export class Message {
         public postedAt: Date = new Date(),
         public lastReplyAt: Date = new Date(),
         public content: string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi odio quia distinctio, a rem tenetur nihil iste saepe voluptates.',
-        public reactions: Reaction[] = [new Reaction('2705.svg', ['Maria Mustermann']), new Reaction]
+        public reactions: Reaction[] = [new Reaction('2705.svg', ['Maria Mustermann']), new Reaction],
+        public replies: Message[] = []
     ) {}
 
     toJson() {
+        // Message interface to be added:
+        const repliesForJson: any[]  = [];
+        if (this.replies.length > 0) {
+            this.replies.forEach(reply => {
+                repliesForJson.push(reply.toJson());
+            })
+        }
         return {
             imageName: this.imageName,
             userName: this.userName,
@@ -19,7 +27,8 @@ export class Message {
             postedAtAsString: this.postedAt.getTime(),
             lastReplyAtAsString: this.lastReplyAt.getTime(),
             content: this.content,
-            reactions: this.reactions
+            reactions: this.reactions,
+            repliesAsString: JSON.stringify(repliesForJson)
         }
     }
 }
