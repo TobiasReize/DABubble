83a810c5e476f0968a090213f0383cf6c46e9547
Christian Frei <christian.frei.01@googlemail.com>
2024-10-15 08:49:04 +0200

    remove firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: 'AIzaSyAewq1pskfBDaEIw8aKVSYsJ6dA9ToZyys',
-    authDomain: 'dabubble-9342d.firebaseapp.com',
-    projectId: 'dabubble-9342d',
-    storageBucket: 'dabubble-9342d.appspot.com',
-    messagingSenderId: '881311515242',
-    appId: '1:881311515242:web:a958501bca7db3c6d24013',
-  },
+  firebaseConfig: { },
 };
