19dfdf76e1db974ec8597806e85ef95981bab40c
Christian Frei <christian.frei.01@googlemail.com>
2024-10-27 00:45:27 +0200

    add another option for handling clicks on mentions

--- src/app/features/main/message-textarea/mention/mention.component.ts
+++ src/app/features/main/message-textarea/mention/mention.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { ChatUser } from '../../../../core/models/user.class';
 
 @Component({
@@ -10,8 +10,20 @@ import { ChatUser } from '../../../../core/models/user.class';
 })
 export class MentionComponent {
   @Input() user!: ChatUser;
+  @ViewChild('mention', { read: ElementRef }) mention!: ElementRef;
 
-  openUserProfile(userUID: string) {
-    console.log('opening user profile', userUID);
+  constructor() { }
+
+  selectMention() {
+    this.mention.nativeElement.setAttribute('contenteditable', true);
+    const selection = window.getSelection();
+    selection?.removeAllRanges();
+    const range = document.createRange();
+    range.selectNodeContents(this.mention.nativeElement);
+    selection?.addRange(range);
   }
+
+  // openUserProfile(userUID: string) {
+  //   console.log('opening user profile', userUID);
+  // }
 }
