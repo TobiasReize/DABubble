a593225a181360bac8a9a0345b63068cbed3108b
Christian Frei <christian.frei.01@googlemail.com>
2024-11-04 18:27:10 +0100

    improve edit channel dialog

--- src/app/features/main/chat/edit-channel/edit-channel.component.ts
+++ src/app/features/main/chat/edit-channel/edit-channel.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, ElementRef, Signal } from '@angular/core';
+import { Component, computed, ElementRef, Signal, ViewChild } from '@angular/core';
 import { ChatService } from '../../../../core/services/chat/chat.service';
 import { Channel } from '../../../../core/models/channel.class';
 import { CommonModule } from '@angular/common';
@@ -18,8 +18,14 @@ export class EditChannelComponent {
   currentChannel: Signal<Channel> = this.chatService.currentChannel;
   channels: Signal<Channel[]> = this.chatService.channels;
   channelNameError: boolean = false;
+  channelDescriptionError: boolean = false;
+  @ViewChild('channelDescriptionInput') channelDescriptionInput!: ElementRef;
 
   constructor(private chatService: ChatService) {}
+
+  ngAfterViewInit() {
+    this.resizeTextArea();
+  }
 
   toggleChannelNameEdit(channelName: string, channelNameInput: HTMLInputElement) {
     if (this.isChannelNameEditable) {
@@ -30,9 +36,11 @@ export class EditChannelComponent {
         channelNameInput.value = this.currentChannel().name;
       } else {
         if (this.isChannelNameEditable) {
-          this.chatService.updateChannel({
-            name: channelName
-          })
+          try {
+            this.chatService.updateChannel({name: channelName});
+          } catch {
+            this.channelNameError = true;
+          }
         }
       }
     } else {
@@ -42,10 +50,18 @@ export class EditChannelComponent {
   }
 
   toggleChannelDescriptionEdit(channelDescription: string) {
+    setTimeout(() => {
+      this.resizeTextArea();
+    }, 0);
     if (this.isChannelDescriptionEditable) {
-      this.chatService.updateChannel({
-        description: channelDescription
-      })
+      this.channelDescriptionError = false;
+      try {
+        this.chatService.updateChannel({description: channelDescription});
+      } catch {
+        this.channelDescriptionError = true;
+      }
+    } else {
+      this.channelDescriptionError = false;
     }
     this.isChannelDescriptionEditable = !this.isChannelDescriptionEditable;
   }
@@ -56,5 +72,13 @@ export class EditChannelComponent {
 
   leaveChannel() {
     this.chatService.leaveChannel();
+    this.closeEditChannel();
+  }
+
+  resizeTextArea() {
+    if (this.channelDescriptionInput) {
+      this.channelDescriptionInput.nativeElement.style.height = '';
+      this.channelDescriptionInput.nativeElement.style.height = this.channelDescriptionInput.nativeElement.scrollHeight + 2 + 'px';
+    }
   }
 }
