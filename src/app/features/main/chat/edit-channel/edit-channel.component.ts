b666e4db986e31f9d9ef8022f8e5b164dd959d16
Christian Frei <christian.frei.01@googlemail.com>
2024-11-04 17:37:42 +0100

    display error message when editing channel name fails

--- src/app/features/main/chat/edit-channel/edit-channel.component.ts
+++ src/app/features/main/chat/edit-channel/edit-channel.component.ts
@@ -1,12 +1,13 @@
-import { Component, computed, Signal } from '@angular/core';
+import { Component, computed, ElementRef, Signal } from '@angular/core';
 import { ChatService } from '../../../../core/services/chat/chat.service';
 import { Channel } from '../../../../core/models/channel.class';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-channel',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './edit-channel.component.html',
   styleUrl: './edit-channel.component.scss'
 })
@@ -15,14 +16,27 @@ export class EditChannelComponent {
   isChannelNameEditable: boolean = false;
   isChannelDescriptionEditable: boolean = false;
   currentChannel: Signal<Channel> = this.chatService.currentChannel;
+  channels: Signal<Channel[]> = this.chatService.channels;
+  channelNameError: boolean = false;
 
   constructor(private chatService: ChatService) {}
 
-  toggleChannelNameEdit(channelName: string) {
+  toggleChannelNameEdit(channelName: string, channelNameInput: HTMLInputElement) {
     if (this.isChannelNameEditable) {
-      this.chatService.updateChannel({
-        name: channelName
-      })
+      this.channelNameError = false;
+      const channel = this.channels().find(channel => channel.name === channelName);
+      if (channel) {
+        this.channelNameError = true;
+        channelNameInput.value = this.currentChannel().name;
+      } else {
+        if (this.isChannelNameEditable) {
+          this.chatService.updateChannel({
+            name: channelName
+          })
+        }
+      }
+    } else {
+      this.channelNameError = false;
     }
     this.isChannelNameEditable = !this.isChannelNameEditable;
   }
