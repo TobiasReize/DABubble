6a7df3e40e1210820c2e3ca0da4db705f2348808
Tobias Reize <reizeto@web.de>
2024-11-04 16:24:19 +0100

    added logout and showProfile-functions in header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { UserService } from '../../core/services/user/user.service';
+import { Router } from '@angular/router';
+import { ChatService } from '../../core/services/chat/chat.service';
 
 @Component({
   selector: 'app-header',
@@ -11,5 +13,30 @@ import { UserService } from '../../core/services/user/user.service';
 export class HeaderComponent {
 
   userService = inject(UserService);
+  chatService = inject(ChatService);
+  profilInfo: boolean = false;
 
+  constructor(private router: Router) { }
+
+
+  toggleProfilInfo() {
+    this.profilInfo = !this.profilInfo;
+  }
+
+
+  showProfile() {
+    this.toggleProfilInfo();    
+    this.chatService.openViewProfile(this.userService.currentUserUID());
+  }
+
+
+  stopPropagation(event: Event) {
+    event.stopPropagation();
+  }
+
+
+  async logout() {
+    await this.userService.signOutUser();
+    this.router.navigateByUrl('');
+  }
 }
